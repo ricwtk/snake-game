3f780bea33b6b614fafec07321013787d5e421a7
Richard Wong <ricwtk@gmail.com>
2021-03-18 13:55:00 +0800

    add js file; ui: basic structure of the main page

---
+++ app/frontend/index.js
@@ -0,0 +1,30 @@
+var vm = new Vue({
+  el: "#app",
+  data: {
+    logs: [
+      "log test log test log test log test log test log test log test ",
+      "log test",
+      "log test",
+      "log test",
+      "log test",
+      "log test",
+      // "log test",
+      // "log test",
+      // "log test",
+      // "log test",
+      // "log test",
+      // "log test",
+      // "log test",
+      // "log test",
+      // "log test",
+      // "log test",
+      // "log test",
+      // "log test",
+      // "log test",
+      // "log test",
+      // "log test",
+      // "log test"
+    ],
+    accPoints: 0,
+  }
+});
